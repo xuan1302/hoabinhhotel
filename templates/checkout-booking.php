71d32f5a758eeb6f2bf0af05ac2ab0c4054986c3
xuan1302 <xuaan.ndx@gmail.com>
2024-08-03 16:41:28 +0700

    bank

--- templates/checkout-booking.php
+++ templates/checkout-booking.php
@@ -7,12 +7,18 @@ $num_adult = sanitize_text_field( $_GET['adult'] );
 $num_child = sanitize_text_field( $_GET['child'] );
 $idsPost = json_decode(wp_unslash($_GET['idsPost']), true);
 $countRoom = json_decode(wp_unslash((wp_unslash($_GET['countRoom']))), true);
+$shortcode = get_field( "shortcode" );
 $args_system_room = array(
     'post_type' => 'rooms',
     'post_status' => 'publish',
     'post__in'   => $idsPost,
 );
 $myposts_system_room = get_posts($args_system_room);
+
+$qr_code = get_field('qr_code', 'option');
+$bank_name = get_field('bank_name', 'option');
+$acc_bank = get_field('acc_bank', 'option');
+$bank_fullname = get_field('bank_fullname', 'option');
 
 ?>
 
@@ -98,7 +104,52 @@ $myposts_system_room = get_posts($args_system_room);
                 <div class="right-content">
                     <div class="form-content-right">
                         <h4 class="title">Điền thông tin liên hệ</h4>
-                        <?php echo do_shortcode('[contact-form-7 id="12af700" title="Form checkout booking"]'); ?>
+                        <?php echo do_shortcode($shortcode); ?>
+                    </div>
+                    <div class="info-prepayment">
+                        <h4 class="title">Thông tin thanh toán Phí đặt trước</h4>
+                        <div class="acc-bank">
+                            <div class="qr-code">
+                                <img src="<?php echo $qr_code['url']; ?>" alt="">
+                                <div class="info-bank">
+                                    <div class="content-info-bank">
+                                        <div class="item">
+                                            <label>Tên ngân hàng</label>
+                                            <b><?php echo $bank_name; ?></b>
+                                        </div>
+                                        <div class="item">
+                                            <label>Số tài khoản</label>
+                                            <b><?php echo $acc_bank; ?></b>
+                                        </div>
+                                        <div class="item">
+                                            <label>Tên chủ tài khoản</label>
+                                            <b><?php echo $bank_fullname; ?></b>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="note">
+                            <h5>LƯU Ý: </h5>
+                            <ul>
+                                <li>Nội dung chuyển khoản ghi số điện thoại đã điền ở thông tin liên hệ.</li>
+                                <li>Chuyển chính xác số tiền Phí đặt trước được ghi ở cột bên phải.</li>
+                                <li>Phí đặt trước sẽ được hoàn lại khi trả phòng.</li>
+                                <li>Tiền Phí đặt trước sẽ không được hoàn lại khi khách hủy đặt phòng. </li>
+                            </ul>
+                        </div>
+                        <div class="booking-success">
+                            <div class="icon">
+                                <img src="<?php bloginfo('template_url'); ?>/asset/icons/icon-succes.png" alt="">
+                            </div>
+                            <div class="text">
+                                Booking của bạn sẽ được xác nhận sau khi bạn thanh toán Phí đặt trước. Chúng tôi sẽ sớm liên hệ với bạn qua số điện thoại.
+                            </div>
+                        </div>
+                        <div class="hotline-booking-success">
+                            Gọi Hotline <b>0238 3746 888</b>  để được hỗ trợ 24/7
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
